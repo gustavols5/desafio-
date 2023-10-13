82401a40c117204e392793dd0b238157b4f68a3d
gustavols5 <gustavoleandro071@gmail.com>
2023-10-13 00:42:04 -0300

    commitFinal

--- classificador.js
+++ classificador.js
@@ -1,57 +1,78 @@
 // Classificador de nível de Herói
-let input = prompt("Digite a xp")
+
+let input = prompt("Digite a xp");
 let xp = parseInt(input);
-let nome = "Super Felipe"
-let classificacao = " " 
+let nome = "Super Felipe";
+let classificacao = "";
 
-switch(xp){
-    case(xp < 1000):
-    classificacao = " Ferro"
-    break
-
-    case(xp > 1000 && xp <= 2000):
-    classificacao = " Bronze"
-    break
-
-    case(xp > 2000 && xp <= 5000):
-    classificacao = " Prata"
-    break
-    
-    case(xp > 6000 && xp <= 7000):
-    classificacao = " Ouro"
-    break
-
-    case(xp > 7000 && xp <= 8000):
-    classificacao = " Platina"
-    break
-
-    case(xp > 8000 && xp <= 9000):
-    classificacao = " Ascendente"
-    break
-
-    case(xp > 9000 && xp <= 10000):
-    classificacao = " Imortal"
-    break
-
-    case(xp <= 10001):
-    classificacao = " Radiante"
-    break
-
+if (xp < 1000) {
+    classificacao = "Ferro";
+} else if (xp > 1000 && xp <= 2000) {
+    classificacao = "Bronze";
+} else if (xp > 2000 && xp <= 5000) {
+    classificacao = "Prata";
+} else if (xp > 6000 && xp <= 7000) {
+    classificacao = "Ouro";
+} else if (xp > 7000 && xp <= 8000) {
+    classificacao = "Platina";
+} else if (xp > 8000 && xp <= 9000) {
+    classificacao = "Ascendente";
+} else if (xp > 9000 && xp <= 10000) {
+    classificacao = "Imortal";
+} else {
+    classificacao = "Radiante";
 }
 
-console.log( "O Herói de nome " + nome + " está no nível " + classificacao);
+console.log("O Herói de nome " + nome + " está no nível " + classificacao);
+
+// Essa foi a primeira tentativa, mas depois de alguns testes ele nao estava imprimindo a classificaçao.
+// Como eram varias variaveis pensei direto no swit-case.
+// Entao depois de uma pesquisa vi que o swit-case nao era recomendado para expressoes booleanas.
+// Entao mudei para esse tanto ai de else e if
 
 
-// Objetivo
-
-//Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
 
 
-//Se XP for entre 7.001 e 8.000 = Platina
-//Se XP for entre 8.001 e 9.000 = Ascendente
-//Se XP for entre 9.001 e 10.000= Imortal
-//Se XP for maior ou igual a 10.001 = Radiante
-// Saída
 
-//Ao final deve se exibir uma mensagem:
-//"O Herói de nome **{nome}** está no nível de **{nivel}**"
+// let input = prompt("Digite a xp")
+// let xp = parseInt(input);
+// let nome = "Super Felipe"
+// let classificacao = " "
+
+// switch(xp){
+//    case(xp < 1000):
+//  classificacao = " Ferro"
+//   break
+
+//    case(xp > 1000 && xp <= 2000):
+//    classificacao = " Bronze"
+//   break
+
+//    case(xp > 2000 && xp <= 5000):
+//    classificacao = " Prata"
+//    break
+    
+//    case(xp > 6000 && xp <= 7000):
+//    classificacao = " Ouro"
+//    break
+//
+//   case(xp > 7000 && xp <= 8000):
+//    classificacao = " Platina"
+//  break
+
+//    case(xp > 8000 && xp <= 9000):
+//   classificacao = " Ascendente"
+//    break
+
+//    case(xp > 9000 && xp <= 10000):
+//    classificacao = " Imortal"
+//   break
+
+//    case(xp <= 10001):
+//   classificacao = " Radiante"
+//    brea
+
+//}
+
+//console.log( "O Herói de nome " + nome + " está no nível " + classificacao);
+
